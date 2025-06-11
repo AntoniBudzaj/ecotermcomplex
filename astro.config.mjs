60e02b57a1677955705af17ad097ea5e317c5fe7
AntoniBudzaj <budzaj37@gmail.com>
2025-06-11 13:32:13 +0200

    i18n translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
 import cloudflare from "@astrojs/cloudflare";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +17,16 @@ export default defineConfig({
     plugins: [
       tailwindcss()
     ],  
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+        "@components": resolve(__dirname, "./src/components"),
+        "@layouts": resolve(__dirname, "./src/layouts"),
+        "@images": resolve(__dirname, "./src/assets/images"),
+        "@styles": resolve(__dirname, "./src/styles"),
+        "@i18n":resolve(__dirname,"./src/I18n")
+      },
+    },
   },
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
@@ -23,6 +38,13 @@ export default defineConfig({
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp'
+    }
+  },
+  i18n: {
+    defaultLocale: "pl",
+    locales: ["pl", "en"],
+    routing: {
+      prefixDefaultLocale: false,
     }
   }
 });
---
+++ src/I18n/ui.ts
@@ -0,0 +1,36 @@
+export const languages = {
+	en: 'English',
+	pl: 'Polski',
+};
+
+export const defaultLang = 'pl';
+
+export const ui = {
+	pl: {
+		'nav.about': 'O nas',
+		'nav.services': 'Usługi',
+		'nav.contact': 'Kontakt',
+		'nav.etcheck': 'ETCheck',
+		'footer.contact': 'Kontakt',
+		'footer.address': 'Adres',
+		'footer.phone': 'Telefon',
+		'footer.email': 'Email',
+		'footer.quicknav': 'Szybkie linki',
+		'footer.social': 'Media Społecznościowe',
+		'footer.rights': 'Wszelkie prawa zastrzeżone',
+		'services.thermal': 'Kompleksowe usługi biogazowe'
+	},
+	en: {
+		'nav.about': 'About us',
+		'nav.services': 'Services',
+		'nav.contact': 'Contact',
+		'nav.etcheck': 'ETCheck',
+		'footer.contact': 'Contact',
+		'footer.address': 'Address',
+		'footer.phone': 'Phone',
+		'footer.email': 'Email',
+		'footer.quicknav': 'Quick navigation',
+		'footer.rights': 'All rights reserved',
+		'services.thermal': 'Comprehensive biogas services'
+	},
+} as const;
